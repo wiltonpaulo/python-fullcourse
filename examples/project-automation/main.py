ccd51616e2b62f4c418ccd4e244e6676334f029e
Wilton Paulo <wiltonpaulo@gmail.com>
2022-03-30 21:09:29 -0300

    Finishing project automation xlsx file

--- examples/project-automation/main.py
+++ examples/project-automation/main.py
@@ -4,3 +4,37 @@ inv_file = openpyxl.load_workbook("inventory.xlsx")
 product_list = inv_file["Sheet1"]
 
 products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_100_inv = {}
+
+# print(product_list.max_row)
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+
+    # calculation number of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        #print("adding a new supplier")
+        products_per_supplier[supplier_name] = 1
+
+    # calculation total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + \
+            inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
+
+    # logic products with inventory under 100
+    if inventory < 100:
+        products_under_100_inv[product_num] = inventory
+
+print(products_per_supplier)
+print(total_value_per_supplier)
+print(products_under_100_inv)
